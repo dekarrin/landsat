7fa35645675323c985a26c7a7a5b149b2d57e101
Rebecca 'TF' Nelson <tails.fanatic@gmail.com>
2014-05-26 10:56:21 -0500

    Updated build toolchain and added autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,83 @@
+#!/bin/sh
+
+# Generates build files
+#
+# Defaults for Autotools may be overrided by setting the environment variable for that program.
+# $LIBTOOLIZE, $ACLOCAL, $AUTOHEADER, $AUTOMAKE, and $AUTOCONF may each be set in the parent
+# environment to use their contents rather than the defaults.
+
+LIBTOOLIZE_NAMES=$LIBTOOLIZE libtoolize glibtoolize
+ACLOCAL_NAMES=$ACLOCAL aclocal
+AUTOHEADER_NAMES=$AUTOHEADER autoheader
+AUTOMAKE_NAMES=$AUTOMAKE automake
+AUTOCONF_NAMES=$AUTOCONF autoconf
+
+# quick func for getting maximum of two numbers
+num_max ()
+{
+	if test "$1" -gt "$2"
+	then
+		echo "$1"
+	else
+		echo "$2"
+	fi
+}
+
+check_program ()
+{
+	working_program=
+	for attempt in "$@"
+	do
+		(${attempt} --version) < /dev/null > /dev/null 2>&1
+		status="$?"
+		if test "$status" = 0
+		then
+			working_program=$attempt
+			break
+		fi
+	done
+	if test -z "$working_program"
+	then
+		echo "Error: could not find a valid program in ($@)"
+	fi
+	echo $working_program
+}
+
+run_program ()
+{
+	echo "$@ ..."
+	"$@" || { echo "Error: $1 failed."; exit 1; }
+}
+
+terminate=0
+
+export status
+
+LIBTOOLIZE=$(check_program "$LIBTOOLIZE_NAMES")
+terminate=$(num_max $status $terminate)
+ACLOCAL=$(check_program "$ACLOCAL_NAMES")
+terminate=$(num_max $status $terminate)
+AUTOHEADER=$(check_program "$AUTOHEADER_NAMES")
+terminate=$(num_max $status $terminate)
+AUTOMAKE=$(check_program "$AUTOMAKE_NAMES")
+terminate=$(num_max $status $terminate)
+AUTOCONF=$(check_program "$AUTOCONF_NAMES")
+terminate=$(num_max $status $terminate)
+
+if test "$terminate" = 1
+then
+	echo "Program check failed."
+	exit 1
+fi
+
+# Otherwise, we tested the environment and have our programs. Time to generate:
+
+git log --date=short --format=format:"%ad %an%n%n	* %s%n" > ChangeLog
+echo >> ChangeLog # git log uses newline as separator between entries, so the last one will not have one
+
+run_program $LIBTOOLIZE --force --copy
+run_program $ACLOCAL
+run_program $AUTOHEADER
+run_program $AUTOMAKE --add-missing
+run_program $AUTOCONF 
+
