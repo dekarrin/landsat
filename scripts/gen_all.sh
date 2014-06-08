bbd7aa8e5eeceab5f4dec912e2439a5a5d988303
Rebecca 'TF' Nelson <tails.fanatic@gmail.com>
2014-06-08 13:55:06 -0500

    Added more scripts for plot generation

---
+++ scripts/gen_all.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# Generates all graphs for an analysis
+
+whereami ()
+{
+	src="${BASH_SOURCE[0]}"
+	while [ -h "$src" ]
+	do
+		dir="$(cd -P "$(dirname "$src")" && pwd)"
+		src="$(readlink "$src")"
+		[[ $src != /* ]] && src="$dir/$src"
+	done
+	cd -P "$(dirname "$src")" && pwd
+}
+
+script_loc="$(whereami)"
+usage_statement="Usage: plot_all.sh [landsat_bin] [red_tif] [near-infrared_tif] [output_dir] [base_name] <args_to_landsat>"
+
+if [ "$#" -lt 1 ]
+then
+	echo "Error: missing landsat binary location" >&2
+	echo "$usage_statemtnt"
+	exit 1
+if [ "$#" -lt 2 ]
+then
+	echo "Error: missing red file name" >&2
+	echo "$usage_statement"
+	exit 1
+elif [ "$#" -lt 3 ]
+then
+	echo "Error: missing near-infrared file name" >&2
+	echo "$usage_statement"
+	exit 1
+elif [ "$#" -lt 4 ]
+then
+	echo "Error: missing output directory" >&2
+	echo "$usage_statement"
+	exit 1
+elif [ "$#" -lt 5 ]
+then
+	echo "Error: missing output file base name" >&2
+	echo "$usage_statement"
+	exit 1
+elif [ ! -x "$1" ]
+	echo "Error: cannot execute '$1'" >&2
+	exit 1
+elif [ ! -f "$2" ]
+then
+	echo "Error: cannot open '$1'" >&2
+	exit 1
+elif [ ! -f "$3" ]
+then
+	echo "Error: cannot open '$2'" >&2
+	exit 1
+elif [ ! -x "$script_loc/plot.sh" ]
+	echo "Error: cannot execute '$script_loc/plot.sh" >&2
+	exit 1
+elif [ ! -x "$script_loc/plot_cells.sh" ]
+	echo "Error: cannot execute '$script_loc/plot_cells.sh" >&2
+	exit 1
+fi
+
+landsat_bin="$1"
+shift
+red_file="$1"
+shift
+nir_file="$1"
+shift
+output_dir="$1"
+shift
+output_base="$1"
+shift
+
+mkdir -p "$output_dir"
+file="$output_dir/$output_base"
+
+"$landsat_bin" -q "$red_file" "$nir_file" "$@" > "${file}_results.txt"
+"$landsat_bin" -qc "$red_file" "$nir_file" "$@" > "${file}_cell_results.txt"
+
+cat "${file}_results.txt" | "$script_loc/plot.sh" -s "${file}_slopes.png"
+cat "${file}_results.txt" | "$script_loc/plot.sh" -i "${file}_intercepts.png"
+cat "${file}_results.txt" | "$script_loc/plot.sh" -r "${file}_r2s.png"
+cat "${file}_results.txt" | \
+ "$script_loc/plot.sh" -s -n "${file}_slopes_noerr.png"
+cat "${file}_results.txt" | \
+ "$script_loc/plot.sh" -i -n "${file}_intercepts_noerr.png"
+cat "${file}_results.txt" | "$script_loc/plot.sh" -r -n "${file}_r2s_noerr.png"
+cat "${file}_cell_results.txt" | \
+ "$script_loc/plot_cells.sh" -s "${file}_cell_slopes.png"
+cat "${file}_cell_results.txt" | \
+ "$script_loc/plot_cells.sh" -i "${file}_cell_intercepts.png"
+cat "${file}_cell_results.txt" | \
+ "$script_loc/plot_cells.sh" -r "${file}_cell_r2s.png"
---
+++ scripts/plot_cells.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# Pipe in the raw data from a -q invocation of landsat.
+
+DATA_FILE="landsat_plot_data.txt"
+
+SLOPES_TITLE="Landsat Red/Near-Infrared Cell Correlation Slopes"
+SLOPES_YTITLE="slope"
+SLOPES_DATA_Y=2
+INTERCEPTS_TITLE="Landsat Red/Near-Infrared Cell Correlation Intercepts"
+INTERCEPTS_YTITLE="intercept"
+INTERCEPTS_DATA_Y=3
+R2S_TITLE="Landsat Red/Near-Infrared Cell Correlation R^2s"
+R2S_YTITLE="R^2"
+R2S_DATA_Y=4
+
+output_file=
+title=$SLOPES_TITLE
+ytitle=$SLOPES_YTITLE
+data_y=$SLOPES_DATA_Y
+
+while [ "$#" -gt 0 ]
+do
+	case "$1" in
+		-*)
+			if [ "$1" = '--slopes' -o "$1" = '-s' ]
+			then
+				title=$SLOPES_TITLE
+				ytitle=$SLOPES_YTITLE
+				data_y=$SLOPES_DATA_Y
+			elif [ "$1" = '--intercepts' -o "$1" = '-i' ]
+			then
+				title=$INTERCEPTS_TITLE
+				ytitle=$INTERCEPTS_YTITLE
+				data_y=$INTERCEPTS_DATA_Y
+			elif [ "$1" = '--r2s' -o "$1" = '-r' ]
+			then
+				title=$R2S_TITLE
+				ytitle=$R2S_YTITLE
+				data_y=$R2S_DATA_Y
+			else
+				echo "Error: unknown option '$1'" >&2
+				exit 1
+			fi
+			;;
+
+		*)
+			if [ -z "$output_file" ]
+			then
+				output_file="$1"
+			fi
+			;;
+	esac
+	shift
+done
+
+if [ -z "$output_file" ]
+then
+	echo "Error: missing output file name" >&2
+	exit 1
+fi
+
+rm -rf "$DATA_FILE"
+while read line
+do
+	x="$(echo $line | cut -d ' ' -f 1)"
+	y="$(echo $line | cut -d ' ' -f $data_y)"
+	echo "$x $y" >> "$DATA_FILE"
+done
+
+gnuplot <<EOF
+output_file="$output_file"
+data_file="$DATA_FILE"
+set terminal png
+set output output_file
+set title "$title"
+set xlabel "window size (pixels)"
+set ylabel "$ytitle"
+set autoscale
+set offsets graph 0.025, graph 0.025, graph 0.025, graph 0.025
+set logscale x 2
+set border back
+set tics out
+plot data_file with linespoints notitle
+EOF
+
+rm -rf "$DATA_FILE"
